ba87fb522ee069a5d49f617662440fc0c169c690
Md <mdakram12022002@gmail.com>
2024-09-20 18:14:03 +0530

    my-app application is created for practice

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Items from './components/Items';
+
+function App() {
+  return (
+    <div>
+      <Items> </Items>
+      <div className="App"> Hello jeee </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/ItemsDate.js
@@ -0,0 +1,11 @@
+import './ItemsDate.css';
+
+function ItemsDate() {
+
+    return (<div>
+        <div className="">16</div>
+        <div className="">Dec</div>
+        <div className="">2001</div>
+    </div>)
+};
+export default ItemsDate;
