034aeb92f1112edb738e0c762f6e603d00e7e1d9
Md <mdakram12022002@gmail.com>
2024-09-21 10:36:35 +0530

    Adding components folder

---
+++ my-shop/src/compnents/Items.js
@@ -0,0 +1,10 @@
+import './Items.css'
+
+function Items() {
+    return (
+        <p className="nirma">Nirma!</p>
+
+
+    )
+}
+export default Items;
---
+++ my-shop/src/compnents/ItemsDate.js
@@ -0,0 +1,13 @@
+import './ItemsDate.css'
+
+function ItemsDate() {
+    return (
+        <div className="mfgDate">
+            <span>16 </span>
+            <span>Dec </span>
+            <span>1999</span>
+        </div>
+    );
+
+}
+export default ItemsDate;
